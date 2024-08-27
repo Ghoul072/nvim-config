64236f2aafbb00bd3eee5901441e788919e7c721
Ghoul072 <ahm3d.yaseen@gmail.com>
2024-08-27 22:57:06 +0500

    refactor: move options to separate file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,55 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-local opt = vim.opt
-
--- [[ Layout and Window Settings ]]
-
--- Sidebar
-opt.number = true
-opt.signcolumn = 'yes'
-
--- Window Split
-opt.splitright = true
-opt.splitbelow = true
-
--- Cursor settings
-opt.scrolloff = 10
-
--- [[ Text Settings ]]
-
--- Save undo history
-opt.undofile = true
-
--- Tab, indent, and space settings
-opt.list = true
-opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-opt.expandtab = true
-opt.smartindent = true
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.breakindent = true
-
--- Search and replace settings
-opt.ignorecase = true
-opt.smartcase = true
-opt.inccommand = 'split'
-
--- Sync system and nvim clipboard
-vim.schedule(function()
-  opt.clipboard = 'unnamedplus'
-end)
-
-
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
---
+++ lua/config/options.lua
@@ -0,0 +1,47 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local opt = vim.opt
+
+-- [[ Layout and Window Settings ]]
+
+-- Sidebar
+opt.number = true
+opt.signcolumn = 'yes'
+
+-- Window Split
+opt.splitright = true
+opt.splitbelow = true
+
+-- Cursor settings
+opt.scrolloff = 10
+
+-- [[ Text Settings ]]
+
+-- Save undo history
+opt.undofile = true
+
+-- Tab, indent, and space settings
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.expandtab = true
+opt.smartindent = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.breakindent = true
+
+-- Search and replace settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.inccommand = 'split'
+
+-- Sync system and nvim clipboard
+vim.schedule(function()
+  opt.clipboard = 'unnamedplus'
+end)
+
+
+
