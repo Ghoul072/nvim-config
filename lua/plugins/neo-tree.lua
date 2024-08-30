43b8a561fb0cf2c0bbecbd343264d7e4387e5ca6
Ghoul072 <ahm3d.yaseen@gmail.com>
2024-08-30 07:55:03 +0500

    feat(neo-tree): add side panel command

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,12 +8,17 @@ return {
     "3rd/image.nvim",
   },
   opts = {
+    close_if_last_window = true,
+    mappings = {
+      ["<space>"] = "none",
+    },
     filesystem = {
       hijack_netrw_behavior = "open_current",
     },
   },
   keys = {
-    { "<leader>e", "<CMD>Neotree toggle float<CR>", silent = true, desc = "Toggle floating file explorer" },
+    { "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Toggle floating file explorer" },
+    { "<leader><Tab>", ":Neotree toggle left<CR>", silent = true, desc = "Toggle left file explorer" },
   },
   lazy = false,
 }
