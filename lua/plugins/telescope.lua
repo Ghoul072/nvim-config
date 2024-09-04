b2514f3301899dde4b761c9a1e313bcb82841be0
Ghoul072 <ahm3d.yaseen@gmail.com>
2024-09-04 17:01:45 +0530

    refactor(telescope): refactor keymaps and remove ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,37 +1,35 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    'nvim-tree/nvim-web-devicons',
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
     local keymap = vim.keymap
-    local telescope = require 'telescope'
-    local builtin = require 'telescope.builtin'
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
-    telescope.setup {
-      extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown()
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
         },
       },
-    }
+    })
 
-    pcall(telescope.load_extension, 'fzf')
-    pcall(telescope.load_extension, 'ui-select')
+    telescope.load_extension("fzf")
 
-    keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[f]ind [h]elp' })
-    keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[f]ind [k]eymaps' })
-    keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[f]ind [f]iles' })
-    keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[f]ind [s]elect Telescope' })
-    keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[f]ind current [w]ord' })
-    keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[f]ind by [g]rep' })
-    keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[f]ind [d]iagnostics' })
-    keymap.set('n', '<leader>fr', builtin.resume, { desc = '[f]ind [r]esume' })
-    keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[f]ind recent files ("." for repeat)' })
-    keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find existing buffers' })
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find current word" })
+    keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find by grep" })
+    keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recent files" })
   end,
 }
