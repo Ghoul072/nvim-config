8b164543ef938b3af936b1a818c9fc6d52934dce
Ghoul072 <ahm3d.yaseen@gmail.com>
2024-09-04 17:31:30 +0530

    feat(alpha): configure greeter

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,33 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button( "n", "  New File" , ":ene <BAR> startinsert <CR>"),
+      dashboard.button( "e", "  File Explorer", ":Neotree toggle current<CR>"),
+      dashboard.button( "f", "  Find File", ":Telescope find_files<CR>"),
+      dashboard.button( "r", "  Recent"   , ":Telescope oldfiles<CR>"),
+      dashboard.button( "q", "  Quit NVIM", ":qa<CR>"),
+    }
+
+    alpha.setup(dashboard.opts)
+    
+    vim.cmd([[
+        autocmd FileType alpha setlocal nofoldenable
+    ]])
+  end,
+}
