12aa53b1ad6c1a02014435a588fa935a46392271
Ghoul072 <ahm3d.yaseen@gmail.com>
2024-09-07 23:02:35 +0530

    fix(bufferline): fix italic text for selected tab

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,11 +2,59 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-      always_show_bufferline = false,
-    },
-  },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        mode = "tabs",
+        separator_style = "slant",
+        always_show_bufferline = false,
+      },
+      highlights = {
+        buffer_selected = {
+          bold = true,
+          italic = false,
+        },
+        numbers_selected = {
+          bold = true,
+          italic = false,
+        },
+        diagnostic_selected = {
+          bold = true,
+          italic = false,
+        },
+        hint_selected = {
+          bold = true,
+          italic = false,
+        },
+        hint_diagnostic_selected = {
+          bold = true,
+          italic = false,
+        },
+        info_selected = {
+          bold = true,
+          italic = false,
+        },
+        info_diagnostic_selected = {
+          bold = true,
+          italic = false,
+        },
+        warning_selected = {
+          bold = true,
+          italic = false,
+        },
+        warning_diagnostic_selected = {
+          bold = true,
+          italic = false,
+        },
+        error_selected = {
+          bold = true,
+          italic = false,
+        },
+        error_diagnostic_selected = {
+          bold = true,
+          italic = false,
+        },
+      },
+    })
+  end,
 }
