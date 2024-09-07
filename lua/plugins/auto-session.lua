b0ead0138137414d26116d75e0f3e140fb1b05f5
Ghoul072 <ahm3d.yaseen@gmail.com>
2024-09-07 07:57:54 +0530

    chore: update auto-session to new config

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,14 +1,16 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    local auto_session = require("auto-session")
-    local keymap = vim.keymap
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
+		local keymap = vim.keymap
 
-    auto_session.setup({
-      auto_restore_enabled = false,
-    })
+		auto_session.setup({
+			auto_restore = false,
+		})
 
-    keymap.set("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session" })
-    keymap.set("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session" })
-  end,
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+		keymap.set("n", "<leader>wr", ":SessionRestore<CR>", { desc = "Restore session" })
+		keymap.set("n", "<leader>ws", ":SessionSave<CR>", { desc = "Save session" })
+	end,
 }
